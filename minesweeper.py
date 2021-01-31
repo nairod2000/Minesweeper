8f3989cd70f12c8583996543f412c077a2cdcde9
nairod2000 <dojmorrissey@gmail.com>
2021-01-30 18:15:12 -0600

    First commit

---
+++ minesweeper.py
@@ -0,0 +1,108 @@
+from tile import Tile
+from random import sample
+import os
+
+class Minesweeper:
+    def __init__(self, rows=16, cols=30, bombs=99, human_player=True):
+        self.rows = rows
+        self.cols = cols
+        self.total_tiles = rows * cols
+        self.bombs = bombs
+        self.bomb_cords = self.generate_bomb_locations()
+        # initalize real board
+        self.board = self._init_board_r()
+
+        self.lost_game = False
+        self.human_player = human_player
+
+    def generate_bomb_locations(self):
+        idxs = sample(range(self.total_tiles), self.bombs)
+        cord_list = list()
+        for idx in idxs:
+            row = idx // self.cols
+            col = idx % self.cols
+            cord_list.append((row, col))
+
+        return cord_list
+
+
+    def _init_board_r(self):
+
+        board = [[Tile() for _ in range(self.cols)] for __ in range(self.rows)]
+        self._populate_bombs(board)
+        self._find_neighbors(board)
+        self._set_display(board)
+        return board
+
+    def _populate_bombs(self, board):
+        # should set __getitem__()
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if (row, col) in self.bomb_cords:
+                    board[row][col].is_bomb = True
+
+    def _find_neighbors(self, board):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                neighbors = 0
+
+                idxs_to_check = [(row-1, col-1), (row-1, col), (row-1, col+1), (row, col-1), (row, col+1), (row+1, col-1), (row+1, col), (row+1, col+1)]
+                # check above
+                for idx in idxs_to_check:
+                    _row, _col = idx
+                    if not (0 <= _row < self.rows) or not (0 <= _col < self.cols):
+                        continue
+                    
+                    try:
+                        if board[_row][_col].is_bomb:
+                            neighbors += 1
+                    except IndexError:
+                        print(f"{_row} {_col}")
+                        quit()
+                
+                board[row][col].neighbors = neighbors
+
+
+    def _set_display(self, board):
+        # return a board of display tiles
+        for row in range(self.rows):
+            for col in range(self.cols):
+                board[row][col].is_display = True
+
+        return board
+
+    def __repr__(self):
+        return str(self)
+        
+    def __str__(self):
+        display_str = ""
+        for i in range(16):
+            display_str += (str(self.board[i]) + "\n")
+        return display_str
+
+    def make_move(self, row, col):
+        self.board[row][col].is_clicked = True
+        
+        if self.board[row][col].is_bomb:
+            self.lost_game = True
+            if self.human_player:
+                self.display(True)
+        elif self.human_player:
+            self.display()
+        
+    def display(self, lost=False):
+        if lost:
+            print("game over")
+
+    def clear_path(self):
+        pass
+
+
+if __name__ == "__main__":
+    m = Minesweeper()
+    while not m.lost_game:
+        print(m)
+        user_input = input("enter corrdinates: ")
+        row, col = int(user_input[0]), int(user_input[2])
+        m.make_move(row, col)
+        print("\033c")
---
+++ tile.py
@@ -0,0 +1,23 @@
+class Tile:
+    def __init__(self, neighbors=None, is_bomb=False, is_display=False):
+        self.neighbors: int = neighbors
+        self.is_bomb: bool = is_bomb
+        self.is_clicked: bool = False
+
+    def __repr__(self):
+        if not self.is_clicked:
+            # unclicked
+            return "U"
+        elif self.is_bomb:
+            # bomb
+            return "B"
+        else:
+            return str(self.neighbors)
+
+    def __str__(self):
+        if not self.is_clicked:
+            return "U"
+        elif self.is_bomb:
+            return "B"
+        else:
+            return str(self.neighbors)
